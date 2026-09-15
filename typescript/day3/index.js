662ae88de6c497b5e01ff7a57525171a1b9e920c
PuneetVerma07 <pk2382849@gmail.com>
2026-09-15 19:35:24 +0530

    generics different usage like in  array and objec

--- typescript/day3/index.js
+++ typescript/day3/index.js
@@ -1,6 +1,23 @@
-const names = ["aman", "gagan"];
-console.log(names);
-const num = [1, 2, 3, 4, 5, 6];
-console.log(num);
+const users = [
+    { name: "Puneet", age: 22 },
+    { name: "aman", age: 23 },
+];
+function firstItem(items) {
+    return items[0];
+}
+const firstUser = firstItem(users);
+console.log(firstUser);
+console.log(firstUser.name);
+console.log(firstUser.age);
+function pair(first, second) {
+    return {
+        first,
+        second,
+    };
+}
+const result = pair("Puneet", 22);
+console.log(result);
+const result2 = pair(true, "Hello");
+console.log(result2);
 export {};
 //# sourceMappingURL=index.js.map
--- typescript/day3/index.ts
+++ typescript/day3/index.ts
@@ -1,5 +1,32 @@
-const names: Array<string> = ["aman", "gagan"];
-console.log(names);
+type User = {
+	name: string;
+	age: number;
+};
 
-const num: Array<number> = [1, 2, 3, 4, 5, 6];
-console.log(num);
+const users: User[] = [
+	{ name: "Puneet", age: 22 },
+	{ name: "aman", age: 23 },
+];
+
+function firstItem<T>(items: T[]): T {
+	return items[0];
+}
+
+const firstUser = firstItem(users);
+
+console.log(firstUser);
+console.log(firstUser.name);
+console.log(firstUser.age);
+
+function pair<T, U>(first: T, second: U) {
+	return {
+		first,
+		second,
+	};
+}
+
+const result = pair("Puneet", 22);
+console.log(result);
+
+const result2 = pair(true, "Hello");
+console.log(result2);
